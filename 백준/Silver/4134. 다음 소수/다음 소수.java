0016d83ff06db1bafe2d4fbb0b30d6b51b6b3ac0
yujin000 <101914200+yujin000@users.noreply.github.com>
2023-10-20 15:48:54 +0900

    [Silver IV] Title: 다음 소수, Time: 704 ms, Memory: 22844 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String src[]) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+
+		for (int i = 0; i < num; i++) {
+			long n = sc.nextLong();
+
+			if (n <= 2) {
+				System.out.println(2);
+				continue;
+			}
+
+			while (true) {
+				boolean prime = true;
+
+				for (int j = 2; j <= Math.sqrt(n); j++) {
+					if (n % j == 0) {
+						prime = false;
+						break;
+					}
+				}
+				if (prime) {
+					System.out.println(n);
+					break;
+				}
+				n++;
+			}
+		}
+		sc.close();
+	}
+}
