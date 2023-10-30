1ad666a5c65af25ac1d704d712906b51945167a4
yujin000 <101914200+yujin000@users.noreply.github.com>
2023-10-30 22:52:11 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 212 ms, Memory: 17680 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String src[]) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		if (a == 60 && b == 60 && c == 60)
+			System.out.println("Equilateral");
+
+		else if (a + b + c == 180) {
+			if (a == b || b == c || c == a) {
+				System.out.println("Isosceles");
+			} else
+				System.out.println("Scalene");
+		} else
+			System.out.println("Error");
+	}
+}
