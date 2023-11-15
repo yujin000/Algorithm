55e5e5b7dc4ef6e7192b86db1fd4a6fb17b5c4cb
yujin000 <101914200+yujin000@users.noreply.github.com>
2023-11-15 23:20:48 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 588 ms, Memory: 24724 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String src[]) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N - i - 1; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < 2 * i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		for (int i = 1; i < N; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < 2 * (N - i) - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
