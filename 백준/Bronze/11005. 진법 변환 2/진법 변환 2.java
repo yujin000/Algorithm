00d8264d475a994aed37669975c24a42e667114f
yujin000 <101914200+yujin000@users.noreply.github.com>
2023-11-27 23:56:47 +0900

    [Bronze I] Title: 진법 변환 2, Time: 136 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String src[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuffer sb = new StringBuffer();
+
+		int N = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+
+		while (N > 0) {
+			if (N % B < 10) {
+				sb.append((char) ((N % B) + '0'));
+			} else {
+				sb.append((char) ((N % B) + 'A' - 10));
+			}
+			N /= B;
+		}
+		System.out.println(sb.reverse());
+	}
+}
