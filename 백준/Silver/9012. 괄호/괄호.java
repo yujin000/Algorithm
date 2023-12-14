c46e8a87f94c7f663bed6ef49b93cd2d07193696
yujin000 <101914200+yujin000@users.noreply.github.com>
2023-12-15 00:53:37 +0900

    [Silver IV] Title: 괄호, Time: 136 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String src[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < T; i++) {
+			String s = br.readLine();
+
+			if (isValid(s)) {
+				System.out.println("YES");
+			} else
+				System.out.println("NO");
+		}
+	}
+
+	private static boolean isValid(String s) {
+		Stack<Character> stack = new Stack();
+
+		for (char c : s.toCharArray()) {
+			if (c == '(') {
+				stack.push(')');
+			} else if (!stack.empty() && stack.peek() == c) {
+				stack.pop();
+			} else {
+				return false;
+			}
+		}
+		return stack.empty();
+	}
+}
