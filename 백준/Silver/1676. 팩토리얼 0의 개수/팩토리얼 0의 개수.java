5ef7f9a8b4b803eb0ef2ba1b511eed043253dfc6
YuJin Shin <101914200+yujin000@users.noreply.github.com>
2024-03-20 21:50:33 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String src[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		int count = 0;
+
+		while (n >= 5) {
+			count += n / 5;
+			n /= 5;
+		}
+		System.out.println(count);
+	}
+}
