697c1dd8ef711df5c31019919753221b766459b2
YuJin Shin <101914200+yujin000@users.noreply.github.com>
2024-03-25 21:34:20 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.39 ms, Memory: 65.8 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -2,12 +2,8 @@ class Solution {
     public String solution(int n) {
         String answer = "";
         for(int i=1;i<=n;i++){
-            if(i%2!=0){
-                answer+="수";
-            }else{
-                answer+="박";
+            answer+= i%2!=0 ? "수" : "박";
             }
+         return answer;
         }
-        return answer;
-    }
 }
