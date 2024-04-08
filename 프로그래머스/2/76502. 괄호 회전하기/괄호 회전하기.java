883010d037015893da510a491302bb75ab280e21
YuJin Shin <101914200+yujin000@users.noreply.github.com>
2024-04-08 23:20:05 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.28 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {
+    private Queue<Character> queue = new LinkedList<>();
+    
+    public int solution(String s) {
+        int answer = 0;
+        
+        for(char c : s.toCharArray()){
+            queue.offer(c);
+        }
+        
+        for(int i=0; i<s.length();i++){
+            if(i==0){
+                answer = correctStack(queue);
+            }else{
+                rotationQueue(queue,i);
+                answer += correctStack(queue);
+            }
+        }
+        return answer;
+    }
+    
+    private void rotationQueue(Queue<Character> queue, int num){
+            char c = queue.poll();
+            queue.offer(c);
+    }
+    
+    private int correctStack(Queue<Character> queue){
+        Stack<Character> stack = new Stack<>();
+        int count = 0;
+        for(char c : queue){
+            if(c=='('){
+                stack.push(')');
+            }else if(c=='{'){
+                stack.push('}');
+            }else if(c=='['){
+                stack.push(']');
+            }else if(!stack.isEmpty()&&stack.peek()==c){
+                stack.pop();
+            }else{
+                return 0;
+            }
+        }
+        return stack.isEmpty() ? 1 : 0;
+    }
+}
