9b1d183067ae10216118c32a4088fc0bbc586a8d
YuJin Shin <101914200+yujin000@users.noreply.github.com>
2024-04-16 18:38:43 +0900

    [Silver II] Title: 랜선 자르기, Time: 188 ms, Memory: 17376 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String src[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());
+		
+		long max = 0;
+		
+		int[] arr = new int[K];
+		for (int i = 0; i < K; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+		
+		max++;
+		
+		long min = 0;
+		long mid = 0;
+		
+		while (min < max) {
+			mid = (min + max) / 2;
+			long count = 0;
+			for (int i = 0; i < arr.length; i++) {
+				count += arr[i] / mid;
+			}
+			if (count < N) {
+				max = mid;
+			} else {
+				min = mid + 1;
+			}
+		}
+		System.out.println(min-1);
+	}
+}
