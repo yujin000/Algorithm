ddea4dcea04519e8b568ff5a32281bddc33abeb9
YuJin Shin <101914200+yujin000@users.noreply.github.com>
2024-04-20 14:57:23 +0900

    [Silver IV] Title: 수 찾기, Time: 552 ms, Memory: 44664 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,33 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+	private static int[] arr;
+
 	public static void main(String src[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 
 		int N = Integer.parseInt(br.readLine());
-		HashMap<Integer, Integer> hashMap = new HashMap<>();
-		
+		arr = new int[N];
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < N; i++) {
-			hashMap.put(Integer.parseInt(st.nextToken()), 0);
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
+		Arrays.sort(arr);
+
 		int M = Integer.parseInt(br.readLine());
-		
+
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < M; i++) {
 			int num = Integer.parseInt(st.nextToken());
-			if (hashMap.containsKey(num)) {
-				sb.append(1).append("\n");
-			} else {
-				sb.append(0).append("\n");
-			}
+			sb.append(lowerBound(num)>=0 ? 1 : 0).append("\n");
 		}
 		System.out.println(sb);
+	}
+
+	private static int lowerBound(int num) {
+		int min = 0;
+		int max = arr.length - 1;
+
+		while (min <= max) {
+			int mid = (min + max) / 2;
+			if (num < arr[mid]) {
+				max = mid - 1;
+			} else if (num > arr[mid]) {
+				min = mid + 1;
+			} else {
+				return mid;
+			}
+		}
+		return -1;
 	}
 }
