7ba3cf6b5284289814835ce1a67ee878649e1fb1
YuJin Shin <101914200+yujin000@users.noreply.github.com>
2024-07-15 23:53:32 +0900

    [level 0] Title: 대문자와 소문자, Time: 18.87 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(char c : my_string.toCharArray()){
+            if(Character.isUpperCase(c)){
+                answer+=Character.toLowerCase(c);
+            }else{
+                answer+=Character.toUpperCase(c);
+            }
+        }
+        return answer;
+    }
+}
