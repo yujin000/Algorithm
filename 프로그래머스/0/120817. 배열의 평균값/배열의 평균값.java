c32c8d1df7304d537ab7ec5a0bf0b0b57f378ad4
YuJin Shin <101914200+yujin000@users.noreply.github.com>
2024-07-23 23:08:02 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        for(double i : numbers){
+            answer+=i;
+        }
+        return answer/numbers.length;
+    }
+}
